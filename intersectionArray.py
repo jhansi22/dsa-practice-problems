f51b92aa061fed30826971739585ced79942fd13
jhansi22 <jhansireddy88@gmail.com>
2026-06-26 12:13:00 +0530

    Update intersection array solution

--- intersectionArray.py
+++ intersectionArray.py
@@ -3,4 +3,7 @@ def intersection_array(nums1, nums2):
     right = 0
     right = []
     while left < len(nums1) and right < len(nums2):
+        left = 0
+        right = 0
+        result = []
         
